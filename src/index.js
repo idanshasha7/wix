9c4d768bb37fbc13fe06f12e00befe7e48848f1c
IdanShasha <idanshasha7@gmail.com>
2017-11-21 12:43:11 +0200

    .
    .

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+const gitHubRepoName = 'wix';
 
 ReactDOM.render(<App />, document.getElementById('app'));
